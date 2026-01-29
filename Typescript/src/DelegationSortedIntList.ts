d7c181fb7bc8ab7d828150e9a5eb535741abef23
Jiening Yang <jieningy@andrew.cmu.edu>
2026-01-29 13:34:12 -0500

    Implement delegation method

--- Typescript/src/DelegationSortedIntList.ts
+++ Typescript/src/DelegationSortedIntList.ts
@@ -7,9 +7,70 @@
  * @author Nora Shoemaker
  *
  */
+import { SortedIntList } from './hidden/SortedIntListLibrary.js'
+import { IntegerList } from './IntegerList.js'
 
 class DelegationSortedIntList {
-  // Write your implementation below with API documentatioin
+  /**
+   * Underlying SortedIntList that stores the integers using delegation.
+   */
+  private sortedIntList: SortedIntList
+  private totalAdded: number
+  
+  /**
+   * Constructor initializes an empty DelegationSortedIntList.
+   */
+  constructor() {
+    this.sortedIntList = new SortedIntList()
+    this.totalAdded = 0
+  }
+
+  /**
+   * Adds the specified int to the list and increments the totalAdded count.
+   *
+   * @param num an integer to be added to the list
+   * @return true if the list is changed as a result of the call
+   */
+  public add(num: number): boolean{
+    this.totalAdded++
+    return this.sortedIntList.add(num)
+  }
+
+  public addAll(list: DelegationSortedIntList): boolean {
+    let success = true
+
+    for (let i = 0; i < list.size(); i++) {
+      success = this.add(list.get(i)) && success
+    }
+
+    return success
+  }
+
+  public getTotalAdded(): number {
+    return this.totalAdded
+  }
+
+  public get(index: number): number {
+    return this.sortedIntList.get(index)
+  }
+
+  public remove(num: number): boolean {
+    return this.sortedIntList.remove(num)
+  }
+
+  public removeAll(list: DelegationSortedIntList): boolean {
+    let success = true
+
+    for (let i = 0; i < list.size(); i++) {
+      success = this.remove(list.get(i)) || success
+    }
+
+    return success
+  }
+
+  public size(): number {
+    return this.sortedIntList.size()
+  }
 }
 
 export { DelegationSortedIntList }
